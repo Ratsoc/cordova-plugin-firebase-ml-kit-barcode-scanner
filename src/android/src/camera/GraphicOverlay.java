36f31dc59c506e4b61a72b1d2304c3d821917bf3
JonK <jonk@costar.ca>
2025-09-11 17:00:38 -0600

    Hardcode CAMERA_FACING values temporarily

--- src/android/src/camera/GraphicOverlay.java
+++ src/android/src/camera/GraphicOverlay.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
-import androidx.camera.core.CameraSelector;
 
 // ----------------------------------------------------------------------------
 // |  Java Imports
@@ -37,13 +36,13 @@ public class GraphicOverlay<T extends GraphicOverlay.Graphic> extends View {
   // ----------------------------------------------------------------------------
   // | Private Properties
   // ----------------------------------------------------------------------------
-  private final Object _Lock              = new Object()                   ;
-  private       int    _PreviewWidth                                       ;
-  private       float  _WidthScaleFactor  = 1.0f                           ;
-  private       int    _PreviewHeight                                      ;
-  private       float  _HeightScaleFactor = 1.0f                           ;
-  private       int    _Facing            = CameraSelector.LENS_FACING_BACK;
-  private       Set<T> _Graphics          = new HashSet<>()                ;
+  private final Object _Lock              = new Object()   ;
+  private       int    _PreviewWidth                       ;
+  private       float  _WidthScaleFactor  = 1.0f           ;
+  private       int    _PreviewHeight                      ;
+  private       float  _HeightScaleFactor = 1.0f           ;
+  private       int    _Facing            = 0              ; // CAMERA_FACING_BACK
+  private       Set<T> _Graphics          = new HashSet<>();
 
   public GraphicOverlay(Context p_Context, AttributeSet p_AttributeSet) {
     super(p_Context, p_AttributeSet);
@@ -140,7 +139,7 @@ public class GraphicOverlay<T extends GraphicOverlay.Graphic> extends View {
     }
 
     public float translateX(float x) {
-      if (_Overlay._Facing == CameraSelector.LENS_FACING_FRONT) {
+      if (_Overlay._Facing == 1) { // CAMERA_FACING_FRONT
         return _Overlay.getWidth() - scaleX(x);
       } else {
         return scaleX(x);
